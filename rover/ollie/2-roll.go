828ad871af3f7e27239a3e96a67625147c45bb3b
deadprogram <ron@hybridgroup.com>
2016-07-10 13:07:59 -0600

    Adds starting for for rover featuring Sphero Ollie
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ rover/ollie/2-roll.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/ble"
+)
+
+func main() {
+	gbot := gobot.NewGobot()
+
+	bleAdaptor := ble.NewBLEClientAdaptor("ble", os.Args[1])
+	ollie := ble.NewSpheroOllieDriver(bleAdaptor, "ollie")
+
+	work := func() {
+    gobot.Every(1*time.Second, func() {
+			r := uint8(gobot.Rand(255))
+			g := uint8(gobot.Rand(255))
+			b := uint8(gobot.Rand(255))
+			ollie.SetRGB(r, g, b)
+		})
+
+		gobot.Every(3*time.Second, func() {
+			ollie.Roll(40, uint16(gobot.Rand(360)))
+		})
+	}
+
+	robot := gobot.NewRobot("ollieBot",
+		[]gobot.Connection{bleAdaptor},
+		[]gobot.Device{ollie},
+		work,
+	)
+
+	gbot.AddRobot(robot)
+
+	gbot.Start()
+}
---
+++ rover/ollie/README.md
@@ -0,0 +1,24 @@
+# Rover - Sphero Ollie
+
+## Installation
+
+```
+go get -d -u github.com/hybridgroup/gobot/... && go install github.com/hybridgroup/gobot/platforms/ble
+```
+
+## Running the code
+When you run any of these examples, you will compile and execute the code on your computer. When you are running the program, you will be communicating with the drone  using the Bluetooth Low Energy (LE) interface.
+
+To compile/run the code:
+
+```
+$ go run 1-color.go BB-128E
+```
+
+Substitute the name of your Ollie or BB-8 as needed.
+
+## Code
+
+### 1-color.go
+
+### 2-roll.go
