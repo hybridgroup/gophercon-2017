770a893276ea5f7dc6cbc651e7ec3cb9d7b0f4a4
Erik Medina <erik@snowcrabsoftware.com>
2017-07-15 10:13:50 -0600

    Update airlock 4 instructions and code.

--- airlock/tinytile/step4.go
+++ airlock/tinytile/step4.go
@@ -41,13 +41,13 @@ func main() {
 	work := func() {
 		Reset()
 
-		button.On(gpio.ButtonPush, func(data interface{}) {
+		button.On(gpio.ButtonRelease, func(data interface{}) {
 			TurnOff()
 			fmt.Println("On!")
 			blue.On()
 		})
 
-		button.On(gpio.ButtonRelease, func(data interface{}) {
+		button.On(gpio.ButtonPush, func(data interface{}) {
 			Reset()
 		})
 	}
