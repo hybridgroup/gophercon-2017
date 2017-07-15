77ce4bb6f8727856779bba582466bc5ad5a31354
Justin Zemlyansky <JustInDevelopment@users.noreply.github.com>
2017-07-15 09:40:42 -0600

    added joystick control

---
+++ starterkit/examples/joystick.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/aio"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+)
+
+func main() {
+	firmataAdaptor := firmata.NewAdaptor("COM59")
+	xAxis := aio.NewAnalogSensorDriver(firmataAdaptor, "0")
+	yAxis := aio.NewAnalogSensorDriver(firmataAdaptor, "1")
+	button := gpio.NewButtonDriver(firmataAdaptor, "13")
+
+	work := func() {
+		xAxis.On(aio.Data, func(data interface{}) {
+			xAxis := data.(int)
+			xAxis = xAxis / 10.0
+			fmt.Println("x axis", xAxis)
+
+		})
+
+		yAxis.On(aio.Data, func(data interface{}) {
+			yAxis := data.(int)
+			yAxis = yAxis / 10.0
+			fmt.Println("y axis", yAxis)
+
+		})
+
+		button.On(gpio.ButtonPush, func(data interface{}) {
+			fmt.Println("button pressed")
+		})
+		button.On(gpio.ButtonRelease, func(data interface{}) {
+			fmt.Println("button released")
+		})
+
+	}
+
+	robot := gobot.NewRobot("sensorBot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{xAxis, yAxis, button},
+		work,
+	)
+
+	robot.Start()
+}
